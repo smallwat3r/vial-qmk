65f59029bfd4a682ca49877a2b3a755b91bcfbe0
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2024-11-02 10:01:27 +0000

    Add support for led

--- keyboards/smallcat/keymaps/vial/keymap.c
+++ keyboards/smallcat/keymaps/vial/keymap.c
@@ -1,5 +1,14 @@
 #include QMK_KEYBOARD_H
 
+/* light up led in red when caps lock in on */
+const rgblight_segment_t PROGMEM capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 1, HSV_RED});
+const rgblight_segment_t* const PROGMEM rgb_layers[] = RGBLIGHT_LAYERS_LIST(capslock_layer);
+void keyboard_post_init_user(void) { rgblight_layers = rgb_layers; }
+bool led_update_user(led_t led_state) {
+    rgblight_set_layer_state(0, led_state.caps_lock);
+    return true;
+}
+
 /* default empty layout */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
