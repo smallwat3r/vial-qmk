d99a3e620ddbd63c1b06f6450e1eb52b9bcca068
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2024-11-03 01:01:09 +0000

    A bit of clean up

--- keyboards/smallcat/keymaps/vial/config.h
+++ keyboards/smallcat/keymaps/vial/config.h
@@ -5,13 +5,14 @@
 #define DYNAMIC_KEYMAP_LAYER_COUNT 8
 #define VIAL_TAP_DANCE_ENTRIES 20
 #define VIAL_COMBO_ENTRIES 40
-#define VIAL_KEYBOARD_UID {0xDA, 0x2A, 0xFA, 0x9D, 0x66, 0xE4, 0x31, 0x52}
 
+#define VIAL_KEYBOARD_UID {0xDA, 0x2A, 0xFA, 0x9D, 0x66, 0xE4, 0x31, 0x52}
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
 #define VIAL_UNLOCK_COMBO_COLS { 1, 4 }
 
 #define NO_ALT_REPEAT_KEY
 
+/* make use of the mounted WS2812 led from the Waveshare RP2040-zero */
 #define RGBLIGHT_LED_COUNT 1
 #define RGBLIGHT_SPLIT
 #define RGBLIGHT_DEFAULT_HUE 110
