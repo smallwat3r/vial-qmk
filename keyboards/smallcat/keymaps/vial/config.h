a5207b4b47844cb787fc3ef1c73c5071fcd1bd70
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2024-11-03 11:31:09 +0000

    Update keymap by making rgb non mandatory

--- keyboards/smallcat/keymaps/vial/config.h
+++ keyboards/smallcat/keymaps/vial/config.h
@@ -13,11 +13,13 @@
 #define NO_ALT_REPEAT_KEY
 
 /* make use of the mounted WS2812 led from the Waveshare RP2040-zero */
-#define RGBLIGHT_LED_COUNT 1
-#define RGBLIGHT_SPLIT
-#define RGBLIGHT_DEFAULT_HUE 110
-#define RGBLIGHT_LIMIT_VAL 10
-#define RGBLIGHT_SLEEP
-#define RGBLIGHT_LAYERS
-#define RGBLIGHT_HUE_STEP 20
-#define WS2812_DI_PIN GP16
+#ifdef RGBLIGHT_ENABLE
+#    define RGBLIGHT_LED_COUNT 1
+#    define RGBLIGHT_SPLIT
+#    define RGBLIGHT_DEFAULT_HUE 110
+#    define RGBLIGHT_LIMIT_VAL 10
+#    define RGBLIGHT_SLEEP
+#    define RGBLIGHT_LAYERS
+#    define RGBLIGHT_HUE_STEP 20
+#    define WS2812_DI_PIN GP16
+#endif
