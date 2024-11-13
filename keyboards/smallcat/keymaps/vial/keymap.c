eb16f0b6a60b7c57feba27faf74ecc10a6f02204
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2024-11-13 09:48:12 +0000

    Add alt repeat implementation

--- keyboards/smallcat/keymaps/vial/keymap.c
+++ keyboards/smallcat/keymaps/vial/keymap.c
@@ -20,6 +20,65 @@
     }
 #endif
 
+enum custom_keycodes {
+    M_UPDIR = SAFE_RANGE,
+    M_ION,
+    M_ENT,
+    M_THE,
+    M_OU,
+    M_EFORE,
+    M_ON,
+    M_UST,
+    M_ER,
+    M_HICH,
+    M_BUT,
+    M_ETURN,
+};
+
+uint16_t get_alt_repeat_key_keycode_user(uint16_t keycode, uint8_t mods) {
+    switch (keycode) {
+        case KC_DOT: return M_UPDIR;
+        case KC_T: return M_ION;
+        case KC_N: return M_ION;
+
+        case KC_SPC:
+        case KC_ENT:
+        case KC_TAB:
+            return M_THE;
+
+        case KC_S: return KC_C;
+        case KC_Y: return M_OU;
+        case KC_I: return M_ON;
+        case KC_B: return M_EFORE;
+        case KC_J: return M_UST;
+        case KC_V: return M_ER;
+        case KC_W: return M_HICH;
+        case KC_COMMA: return M_BUT;
+        case KC_MINUS: return KC_GT;
+        case KC_EQUAL: return KC_GT;
+        case KC_R: return M_ETURN;
+    }
+    return KC_TRNS;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+    switch (keycode) {
+        case M_UPDIR: SEND_STRING("./"); break;
+        case M_ION: SEND_STRING("ion"); break;
+        case M_THE: SEND_STRING("The"); break;
+        case M_ENT: SEND_STRING("ent"); break;
+        case M_OU: SEND_STRING("ou"); break;
+        case M_ON: SEND_STRING("on"); break;
+        case M_EFORE: SEND_STRING("efore"); break;
+        case M_UST: SEND_STRING("ust"); break;
+        case M_ER: SEND_STRING("er"); break;
+        case M_HICH: SEND_STRING("hich"); break;
+        case M_BUT: SEND_STRING(" but"); break;
+        case M_ETURN: SEND_STRING("eturn"); break;
+    }
+    return true;
+}
+
 /* default empty layout */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
