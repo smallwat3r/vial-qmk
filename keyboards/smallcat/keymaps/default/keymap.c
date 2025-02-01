c144319805cccb6549331e80930edd0c28c8dfb5
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2025-02-01 00:43:02 +0000

    WIP

--- keyboards/smallcat/keymaps/default/keymap.c
+++ keyboards/smallcat/keymaps/default/keymap.c
@@ -25,6 +25,7 @@ enum layers {
   SYM,
   NUM,
   NAV,
+  NAV2,
   EDIT,
   FUN,
   FRENCH,
@@ -33,48 +34,38 @@ enum layers {
 
 /* define one name per key, this is a useful level of abstraction */
 /* in order to change combos and edit the config */
-#define K_1 MT(KC_L, KC_L)
-#define K_2 MT(KC_D, KC_D)
-#define K_3 MT(KC_M, KC_M)
-#define K_4 MT(KC_H, MOD_LSFT)
-#define K_5 LT(SYM, KC_R)
-#define K_6 LT(NUM, KC_T)
-#define K_7 LT(NAV, KC_S)
-#define K_8 MT(KC_G, KC_G)
-#define K_9 MT(KC_TAB, MOD_LCTL)
-#define K_10 MT(KC_QUOT, MOD_LALT)
-#define K_11 MT(KC_C, MOD_LGUI)
-#define K_12 LT(KC_NO, EDIT)
-#define K_13 LT(FUN, KC_SPC)
-#define K_14 MT(KC_F, KC_F)
-#define K_15 MT(KC_O, KC_O)
-#define K_16 MT(KC_U, KC_U)
-#define K_17 MT(KC_Y, KC_U)
-#define K_18 LT(FRENCH, KC_N)
-#define K_19 LT(NUM, KC_A)
-#define K_20 LT(SYM, KC_E)
-#define K_21 MT(KC_I, MOD_LSFT)
-#define K_22 MT(KC_P, MOD_LGUI)
-#define K_23 MT(KC_COMM, MOD_LALT)
-#define K_24 MT(KC_DOT, MOD_LCTL)
-#define K_25 LT(SYS, KC_BSPC)
-#define K_26 LT(SYM, KC_ENT)
-
-/* enum custom_keycodes { */
-/*     K_ION = SAFE_RANGE, */
-/*     K_YOU, */
-/*     K_SH, */
-/*     K_TH, */
-/*     K_VER, */
-/*     K_ING, */
-/* }; */
-
+#define CK_1 MT(KC_L, KC_L)
+#define CK_2 MT(KC_D, KC_D)
+#define CK_3 MT(KC_M, KC_M)
+#define CK_4 MT(KC_H, MOD_LSFT)
+#define CK_5 LT(SYM, KC_R)
+#define CK_6 LT(NUM, KC_T)
+#define CK_7 LT(NAV, KC_S)
+#define CK_8 MT(KC_G, KC_G)
+#define CK_9 MT(KC_TAB, MOD_LCTL)
+#define CK_10 MT(KC_QUOT, MOD_LALT)
+#define CK_11 MT(KC_C, MOD_LGUI)
+#define CK_12 LT(KC_NO, EDIT)
+#define CK_13 LT(FUN, KC_SPC)
+#define CK_14 MT(KC_F, KC_F)
+#define CK_15 MT(KC_O, KC_O)
+#define CK_16 MT(KC_U, KC_U)
+#define CK_17 MT(KC_Y, KC_U)
+#define CK_18 LT(FRENCH, KC_N)
+#define CK_19 LT(NUM, KC_A)
+#define CK_20 LT(SYM, KC_E)
+#define CK_21 MT(KC_I, MOD_LSFT)
+#define CK_22 MT(KC_P, MOD_LGUI)
+#define CK_23 MT(KC_COMM, MOD_LALT)
+#define CK_24 MT(KC_DOT, MOD_LCTL)
+#define CK_25 LT(SYS, KC_BSPC)
+#define CK_26 LT(SYM, KC_ENT)
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case K_5:
-        case K_7:
-        case K_20:
+        case CK_5:
+        case CK_7:
+        case CK_20:
           return 80;
         default: return TAPPING_TERM;  // 100
     }
@@ -82,48 +73,84 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT(
-              K_1, K_2, K_3,               K_14, K_15, K_16,
-         K_4, K_5, K_6, K_7, K_8,    K_17, K_18, K_19, K_20, K_21,
-              K_9, K_10, K_11,             K_22, K_23, K_24,
-                        K_12, K_13, K_25, K_26
+               CK_1,  CK_2,  CK_3,                CK_14, CK_15, CK_16,
+         CK_4, CK_5,  CK_6,  CK_7,  CK_8,  CK_17, CK_18, CK_19, CK_20, CK_21,
+               CK_9,  CK_10, CK_11,               CK_22, CK_23, CK_24,
+                          CK_12, CK_13,    CK_25, CK_26
     ),
     [NUM] = LAYOUT(
-              KC_7, KC_8, KC_9,                KC_7, KC_8, KC_9,
-        KC_0, KC_4, KC_5, KC_6, KC_NO,  KC_NO, KC_4, KC_5, KC_6, KC_0,
-              KC_1, KC_2, KC_3,                KC_1, KC_2, KC_3,
-                        KC_NO, KC_SPC,  KC_BSPC, KC_DEL
+               KC_7, KC_8, KC_9,                KC_7, KC_8, KC_9,
+        KC_0,  KC_4, KC_5, KC_6, KC_NO,  KC_NO, KC_4, KC_5, KC_6, KC_0,
+               KC_1, KC_2, KC_3,                KC_1, KC_2, KC_3,
+                       KC_TRNS, KC_SPC,  KC_BSPC, KC_DEL
+    ),
+    [SYM] = LAYOUT(
+              KC_AT, LSFT(KC_3), LSFT(KC_4),                   KC_BSLS, KC_SLASH, LSFT(KC_SLASH),
+ LSFT(KC_8), LSFT(KC_EQUAL), KC_EQUAL, LSFT(KC_QUOTE), LSFT(KC_5), LSFT(KC_1), LSFT(KC_SCLN), RALT(KC_3), LSFT(KC_GRAVE), LSFT(KC_BSLS),
+              KC_MINUS, LSFT(KC_7), KC_GRAVE,                  LSFT(KC_COMMA), LSFT(KC_DOT), KC_NO,
+                                KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS
+    ),
+    [NAV] = LAYOUT(
+                 KC_TRNS, KC_TRNS, KC_TRNS,                     LSFT(KC_LBRC), LSFT(KC_RBRC), KC_TRNS,
+        KC_TRNS, KC_TRNS, MO(NAV2), KC_TRNS, KC_TRNS,   KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_TRNS,
+                 KC_TRNS, KC_TRNS, KC_TRNS,                     KC_LBRC, KC_RBRC, KC_TRNS,
+                                KC_TRNS, KC_TRNS,      KC_BSPC, KC_DEL
+    ),
+    [NAV2] = LAYOUT(
+                 KC_TRNS, KC_TRNS, KC_TRNS,                    KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  LALT(KC_LEFT), KC_TRNS, KC_TRNS, LALT(KC_RIGHT), KC_TRNS,
+                 KC_TRNS, KC_TRNS, KC_TRNS,                    KC_TRNS, KC_TRNS, KC_TRNS,
+                                KC_TRNS, KC_TRNS,     LALT(KC_BSPC), LALT(KC_DEL)
+    ),
+    [EDIT] = LAYOUT(
+                 SGUI(KC_G), LGUI(KC_F), LGUI(KC_G), LSFT(KC_LBRC), LSFT(KC_RBRC), KC_TRNS,
+        LGUI(KC_A), LGUI(KC_X), LT(NAV2, LGUI(KC_C)), LGUI(KC_V), LCTL(KC_C),  KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_TRNS,
+                 KC_TRNS, LGUI(KC_Z), SGUI(KC_Z),                    KC_LBRC, KC_RBRC, KC_TRNS,
+                                KC_TRNS, KC_TRNS,      KC_BSPC, KC_DEL
+    ),
+    [FUN] = LAYOUT(
+                 KC_TRNS, KC_TRNS, KC_TRNS,                    KC_F7, KC_F8, KC_F9,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_F4, KC_F5, KC_F6, KC_TRNS,
+                 KC_TRNS, KC_TRNS, KC_TRNS,                    KC_F1, KC_F2, KC_F3,
+                                KC_TRNS, KC_TRNS,      LALT(KC_BSPC), LALT(KC_DEL)
+    ),
+    [SYS] = LAYOUT(
+                 KC_TRNS, KC_BTN4, KC_BTN5,                    KC_VOLD, KC_VOLU, KC_MUTE,
+        RGB_HUD, KC_BRIU, KC_WH_U, KC_WH_D, KC_TRNS,  KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, RGB_HUI,
+                 KC_BRID, RGB_SAD, RGB_SAI,                   LGUI(KC_KP_MINUS), LGUI(KC_KP_PLUS), KC_TRNS,
+                                KC_BTN2, KC_BTN1,      KC_TRNS, KC_TRNS
     ),
 };
 
 tap_dance_action_t tap_dance_actions[] = {};
 
 // left base
-const uint16_t PROGMEM combo_x[] = {K_1, K_2, COMBO_END};
-const uint16_t PROGMEM combo_q[] = {K_2, K_3, COMBO_END};
-const uint16_t PROGMEM combo_w[] = {K_5, K_6, COMBO_END};
-const uint16_t PROGMEM combo_th[] = {K_6, K_7, COMBO_END};
-const uint16_t PROGMEM combo_z[] = {K_9, K_10, COMBO_END};
-const uint16_t PROGMEM combo_v[] = {K_10, K_11, COMBO_END};
-const uint16_t PROGMEM combo_b[] = {K_6, K_11, COMBO_END};
-const uint16_t PROGMEM combo_lbrc[] = {K_1, K_3, COMBO_END};
-const uint16_t PROGMEM combo_sh[] = {K_5, K_7, COMBO_END};
-const uint16_t PROGMEM combo_lpar[] = {K_9, K_11, COMBO_END};
-const uint16_t PROGMEM combo_ver[] = {K_4, K_7, COMBO_END};
+const uint16_t PROGMEM combo_x[] = {CK_1, CK_2, COMBO_END};
+const uint16_t PROGMEM combo_q[] = {CK_2, CK_3, COMBO_END};
+const uint16_t PROGMEM combo_w[] = {CK_5, CK_6, COMBO_END};
+const uint16_t PROGMEM combo_th[] = {CK_6, CK_7, COMBO_END};
+const uint16_t PROGMEM combo_z[] = {CK_9, CK_10, COMBO_END};
+const uint16_t PROGMEM combo_v[] = {CK_10, CK_11, COMBO_END};
+const uint16_t PROGMEM combo_b[] = {CK_6, CK_11, COMBO_END};
+const uint16_t PROGMEM combo_lbrc[] = {CK_1, CK_3, COMBO_END};
+const uint16_t PROGMEM combo_sh[] = {CK_5, CK_7, COMBO_END};
+const uint16_t PROGMEM combo_lpar[] = {CK_9, CK_11, COMBO_END};
+const uint16_t PROGMEM combo_ver[] = {CK_4, CK_7, COMBO_END};
 // right base
-const uint16_t PROGMEM combo_j[] = {K_14, K_15, COMBO_END};
-const uint16_t PROGMEM combo_esc[] = {K_15, K_16, COMBO_END};
-const uint16_t PROGMEM combo_osmsft[] = {K_18, K_19, COMBO_END};
-const uint16_t PROGMEM combo_underscore[] = {K_19, K_20, COMBO_END};
-const uint16_t PROGMEM combo_scln[] = {K_23, K_24, COMBO_END};
-const uint16_t PROGMEM combo_k[] = {K_19, K_22, COMBO_END};
-const uint16_t PROGMEM combo_rbrc[] = {K_14, K_16, COMBO_END};
-const uint16_t PROGMEM combo_rpar[] = {K_22, K_24, COMBO_END};
-const uint16_t PROGMEM combo_dash[] = {K_18, K_20, COMBO_END};
-const uint16_t PROGMEM combo_you[] = {K_16, K_18, COMBO_END};
-const uint16_t PROGMEM combo_ion[] = {K_18, K_20, COMBO_END};
-const uint16_t PROGMEM combo_ing[] = {K_18, K_21, COMBO_END};
+const uint16_t PROGMEM combo_j[] = {CK_14, CK_15, COMBO_END};
+const uint16_t PROGMEM combo_esc[] = {CK_15, CK_16, COMBO_END};
+const uint16_t PROGMEM combo_osmsft[] = {CK_18, CK_19, COMBO_END};
+const uint16_t PROGMEM combo_underscore[] = {CK_19, CK_20, COMBO_END};
+const uint16_t PROGMEM combo_scln[] = {CK_23, CK_24, COMBO_END};
+const uint16_t PROGMEM combo_k[] = {CK_19, CK_22, COMBO_END};
+const uint16_t PROGMEM combo_rbrc[] = {CK_14, CK_16, COMBO_END};
+const uint16_t PROGMEM combo_rpar[] = {CK_22, CK_24, COMBO_END};
+const uint16_t PROGMEM combo_dash[] = {CK_18, CK_20, COMBO_END};
+const uint16_t PROGMEM combo_you[] = {CK_16, CK_18, COMBO_END};
+const uint16_t PROGMEM combo_ion[] = {CK_18, CK_20, COMBO_END};
+const uint16_t PROGMEM combo_ing[] = {CK_18, CK_21, COMBO_END};
 // both base
-const uint16_t PROGMEM combo_caps[] = {K_8, K_17, COMBO_END};
+const uint16_t PROGMEM combo_caps[] = {CK_8, CK_17, COMBO_END};
 
 enum combo_events {
   CE_LPAR,
@@ -143,7 +170,7 @@ combo_t key_combos[] = {
     // left base
     COMBO(combo_x, KC_X),
     COMBO(combo_q, KC_Q),
-    COMBO(combo_w, KC_Z),
+    COMBO(combo_w, KC_W),
     [CE_TH] = COMBO_ACTION(combo_th),
     COMBO(combo_z, KC_Z),
     COMBO(combo_v, KC_V),
@@ -184,25 +211,3 @@ void process_combo_event(uint16_t combo_index, bool pressed) {
     case CE_ING: if (pressed) { SEND_STRING("ing"); } break;
   }
 }
-
-/* bool process_record_user(uint16_t keycode, keyrecord_t *record) { */
-/*     if (keycode == K_TH && record->event.pressed) { */
-/*         SEND_STRING("th"); return false; */
-/*     } */
-/*     if (keycode == K_SH && record->event.pressed) { */
-/*         SEND_STRING("sh"); return false; */
-/*     } */
-/*     if (keycode == K_VER && record->event.pressed) { */
-/*         SEND_STRING("ver"); return false; */
-/*     } */
-/*     if (keycode == K_YOU && record->event.pressed) { */
-/*         SEND_STRING("you"); return false; */
-/*     } */
-/*     if (keycode == K_ION && record->event.pressed) { */
-/*         SEND_STRING("ion"); return false; */
-/*     } */
-/*     if (keycode == K_ING && record->event.pressed) { */
-/*         SEND_STRING("ing"); return false; */
-/*     } */
-/*     return true; */
-/* } */
